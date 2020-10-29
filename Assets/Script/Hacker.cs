559ff0f1e3ceb04b102f5d06f456048b67e6f818
Daniel Jimenez <danieldavidjp@gmail.com>
2020-10-29 00:05:56 -0500

    Main Menu and Variables

---
+++ Assets/Script/Hacker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        ShowMainMenu("Hello There");
+    }
+
+    void ShowMainMenu(string greetings) 
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine(greetings);
+        Terminal.WriteLine("Is your time to hack into their system Choose the person to hack:");
+        Terminal.WriteLine("1. Secretary");
+        Terminal.WriteLine("2. Manager");
+        Terminal.WriteLine("3. CEO");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
